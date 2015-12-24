28e58f778c99e5f096b5924ce8fec58e4c4ec337
Beto23 <josee-45@hotmail.com>
2015-12-24 02:07:24 -0600

    it have added a image for each Pokemon

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -4,7 +4,7 @@
 	var $Pokemons = $('.Pokemons');
 	var PokeCard = [
 		'<article class="Pokemon">',
-			'<figure class="Pokemon-img"><img></figure>',
+			'<figure class="Pokemon-img" id="imgPokemon"><img src=":img:"></figure>',
 			'<div class="Pokemon-info">',
 				'<h2 class="Pokemon-info-name">:name:</h2>',
 				'<button> <b>More info</b></button>',
@@ -13,7 +13,13 @@
 	].join('');
 
 	pokemons.forEach(function(p){
-		var card = PokeCard.replace(':name:', p.name);
+		var nomb = p.name;
+		var res = '../api/img/'+nomb.toLowerCase()+'.jpg';
+		var card = PokeCard
+			.replace(':name:', p.name)
+			.replace(':img:', '../api/img/'+p.name.toLowerCase()+'.jpg')
+		//console.log(res);
+		//$('.Pokemon-img').attr('backgroundImage','../api/img/abra.jpg');
 		$('.Pokemons').append(card);
 	});
 })()
